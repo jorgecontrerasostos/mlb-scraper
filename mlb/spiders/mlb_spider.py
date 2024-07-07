ad1a03b545b3c998f2ce63c65e00a7400b4f63aa
jorgecontrerasostos <jorge.contreras@justia.com>
2024-07-06 20:36:25 -0600

    add mlb teams

--- mlb/spiders/mlb_spider.py
+++ mlb/spiders/mlb_spider.py
@@ -1,12 +1,14 @@
 import scrapy
 
 
-class MlbSpiderSpider(scrapy.Spider):
+class MLBTeamsSpider(scrapy.Spider):
     name = "mlb_spider"
     allowed_domains = ["www.mlb.com"]
-    start_urls = ["https://www.mlb.com/redsox/roster", "https://www.mlb.com/rays/roster"]
+    teams: [str] = ['redsox', 'yankees', 'rays', 'orioles']
+    start_urls = [f"https://www.mlb.com/{team}/roster" for team in teams]
 
     def parse(self, response):
         yield {
+            "name": response.url.split("/")[-2],
             "players": response.css('td.info a::text').getall()
         }
