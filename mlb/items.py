0bdf3db43927c3bea82e909db8b91387ac9d7c36
jorgecontrerasostos <jorge.contreras@justia.com>
2024-07-06 22:14:48 -0600

    add items

--- mlb/items.py
+++ mlb/items.py
@@ -6,7 +6,8 @@
 import scrapy
 
 
-class MlbItem(scrapy.Item):
+class MlbTeam(scrapy.Item):
     # define the fields for your item here like:
-    # name = scrapy.Field()
-    pass
+    name = scrapy.Field()
+    players = scrapy.Field()
+
