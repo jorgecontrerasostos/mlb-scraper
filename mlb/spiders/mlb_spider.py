fe27709e47e240566ea6cc7945041b532c007aac
jorgecontrerasostos <jorge.contreras@justia.com>
2024-07-06 20:45:32 -0600

    add pending teams

--- mlb/spiders/mlb_spider.py
+++ mlb/spiders/mlb_spider.py
@@ -4,11 +4,42 @@ import scrapy
 class MLBTeamsSpider(scrapy.Spider):
     name = "mlb_spider"
     allowed_domains = ["www.mlb.com"]
-    teams: [str] = ['redsox', 'yankees', 'rays', 'orioles']
+    teams: [str] = [
+        "redsox",
+        "yankees",
+        "rays",
+        "orioles",
+        "bluejays",
+        "whitesox",
+        "guardians",
+        "tigers",
+        "royals",
+        "twins",
+        "astros",
+        "angels",
+        "athletics",
+        "mariners",
+        "rangers",
+        "braves",
+        "marlins",
+        "mets",
+        "phillies",
+        "nationals",
+        "cubs",
+        "reds",
+        "brewers",
+        "pirates",
+        "cardinals",
+        "dbacks",
+        "rockies",
+        "dodgers",
+        "padres",
+        "giants",
+    ]
     start_urls = [f"https://www.mlb.com/{team}/roster" for team in teams]
 
     def parse(self, response):
         yield {
             "name": response.url.split("/")[-2],
-            "players": response.css('td.info a::text').getall()
+            "players": response.css("td.info a::text").getall(),
         }
